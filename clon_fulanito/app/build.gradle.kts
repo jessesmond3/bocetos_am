28a8d3fdcbbcd8868a84b448e7b762e11885390d
jessesmond3 <al213178@alumnos.uacj.mx>
2025-03-28 09:52:44 -0700

    [Agregar cambios 28.03.25]

---
+++ clon_fulanito/app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.clon_fulanito"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.clon_fulanito"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
+//librerias para retrofitval
+val version_retrofit = "2.9.0"
+implementation("com.squareup.retrofit2:retrofit3:${version_retrofit}")
+implementation("com.squareup.retrofit2:converter-gson:${version_retrofit}")
+// libreria para usar programacion asincronicaval
+val version_courtinas = "1.5.1"
+implementation("org.jetbrains.kotlin:kotlinx-cortines-android:${version_courtinas}")
+//libreria para checar la info irl
+implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
